1782d404611ec60d03e732b268d8d1773d8c262e
Hall1110 <160109460+Hall1110@users.noreply.github.com>
2024-08-12 14:30:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void RunProgram()
+        {
+            bool programRunning = true;
+            Console.WriteLine("");
+            Console.ReadLine();
+            do
+            {
+
+            } while (programRunning);
+        }
+
+
+        static decimal Addition(decimal firstValue, decimal secondValue)
+        {
+            
+            
+            return firstValue + secondValue;
+        }
+
+        static decimal Subtraction(decimal firstValue, decimal secondValue)
+        {
+
+
+            return firstValue - secondValue;
+        }
+
+        static decimal Multiplication(decimal firstValue, decimal secondValue)
+        {
+
+
+            return firstValue * secondValue;
+        }
+
+        static decimal Division(decimal firstValue, decimal secondValue)
+        {
+            if (firstValue == 0 || secondValue == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return firstValue / secondValue;
+            }
+        }
+
+        static void CheckInput(string input)
+        {
+
+        }
+    }
+}
